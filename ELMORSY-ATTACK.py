404a60863b04e23064ba6d4573382f7346397ad4
AS7ABELMGRA <79116899+AS7ABELMGRA@users.noreply.github.com>
2021-05-21 15:12:35 +0200

    Update ELMORSY-ATTACK.py

--- ELMORSY-ATTACK.py
+++ ELMORSY-ATTACK.py
@@ -21,10 +21,10 @@ print("SSS 88 888888888888888 88 SSSSS 8888 DDOSS ATTACK ELMORSY          8888 "
 print("SSS 88 888888888888888 88 SSSSS 8888 So for Palestine beloved      8888 ")
 print("SSS 88 oooooooooo      88 SSSSS 8888 ELmorsy supports              8888 ")
 print("SSS 88 8888888888 .::. 88 SSSSS 8888   the Palestinian cause       8888 ")
-print("SSS 88 oooooooooo :::: 88 SSSSS 8888                               8888 ")
-print("SSS 88 8888888888  `   88 SSSSS 8888                               8888 ")
-print("SSS 88ooooooooooooooooo88 SSSS 8888 سكرتير ابليس/عبدالله المرسي يتضامن معا فلسطين
-print("SSS 888888888888888888888  SSSS 8888 عبدالله المرسي يقول كسم اسرائيل وباعلي صوت
+print("SSS 88 oooooooooo :::: 88 SSSSS 8888  KOSOM ISRAEL                 8888 ")
+print("SSS 88 8888888888  `   88 SSSSS 8888  FAKYOU ISRAEL                8888 ")
+print("SSS 88ooooooooooooooooo88 SSSSS 8888                               8888 ")
+print("SSS 888888888888888888888 SSSSS 8888                               8888 ")
 print("SSS |   *  *  *   )8c8888   SSSS 888888888888888888888888888888888888888 ")
 print("SSS 888888888888888888888.  SSS 888888888888888888888888888888888888888 ")
 print("SSS 888888888888888888888 \_ SSsssss oooooooooooooooooooooooooooo ssss ")
